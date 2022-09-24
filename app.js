1ce6c2a23b4870e31cb63a71113faabca726116d
iamkevinjames <75653990+iamkevinjames@users.noreply.github.com>
2022-09-25 00:53:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const Joi = require('@hapi/joi');
 const movies = require('./users');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/user', movies);
