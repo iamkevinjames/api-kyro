6e6a92837927abd5845b325b3dac06c4b71725fa
iamkevinjames <75653990+iamkevinjames@users.noreply.github.com>
2022-09-25 00:42:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 const express = require('express');
 const app = express();
 const Joi = require('@hapi/joi');
-const movies = require('./movies');
+const movies = require('./users');
 
 app.use(express.json());
 
-app.use('/abc', movies);
+app.use('/users', users);
 
 
 app.get('/', (req,res) => {
-    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
+    res.send('You are almost there!');
 })
-
-
-
-
-
-
-
-
-
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
